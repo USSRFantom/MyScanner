480485a14c5e6216cd15ea06b89fe1787efb1fc9
Ussrfantom <dimkakrohin@mail.ru>
2021-05-17 22:28:41 +0300

    added activities with payment

---
+++ app/src/main/java/ussrfantom/com/example/myscanner/Screens/Payment/EquaringActivity.java
@@ -0,0 +1,16 @@
+package ussrfantom.com.example.myscanner.Screens.Payment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ussrfantom.com.example.myscanner.R;
+
+public class EquaringActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_equaring);
+    }
+}
--- app/src/main/java/ussrfantom/com/example/myscanner/Screens/QrCode/QrCode.java
+++ app/src/main/java/ussrfantom/com/example/myscanner/Screens/QrCode/QrCode.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,6 +16,9 @@ import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 import ussrfantom.com.example.myscanner.R;
+import ussrfantom.com.example.myscanner.Screens.Payment.CashActivity;
+import ussrfantom.com.example.myscanner.Screens.Payment.EquaringActivity;
+import ussrfantom.com.example.myscanner.Screens.Payment.NoCashActivity;
 
 public class QrCode extends AppCompatActivity {
 
@@ -22,6 +26,9 @@ public class QrCode extends AppCompatActivity {
     private ImageView imageViewQr;
     private String pay;
     TextView textViewPrice;
+    TextView textViewCash;
+    TextView textViewNoCash;
+    TextView textViewEquaring;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,11 @@ public class QrCode extends AppCompatActivity {
         setContentView(R.layout.activity_qr_code);
         textViewPrice = findViewById(R.id.textViewPrice);
         imageViewQr = findViewById(R.id.imageViewqr_code);
+        textViewCash = findViewById(R.id.textViewPay1);
+        textViewNoCash = findViewById(R.id.textViewPay2);
+        textViewEquaring = findViewById(R.id.textViewPay3);
+
+
         Intent intent = getIntent();
         qr = intent.getStringExtra("Key");
         pay = intent.getStringExtra("Key2");
@@ -36,7 +48,44 @@ public class QrCode extends AppCompatActivity {
         String a = pay + " " + "руб.";
         textViewPrice.setText(a);
         getCode();
+
+        //нажатие на первый текст для оплаты
+        textViewCash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(QrCode.this, CashActivity.class);
+                intent.putExtra("Key", qr);
+                intent.putExtra("Key2", pay);
+                startActivity(intent);
+            }
+        });
+
+        //нажатие на второй текст для оплаты
+        textViewNoCash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(QrCode.this, NoCashActivity.class);
+                intent.putExtra("Key", qr);
+                intent.putExtra("Key2", pay);
+                startActivity(intent);
+            }
+        });
+
+        //нажатие на третий текст для оплаты
+        textViewEquaring.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(QrCode.this, EquaringActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
+
+
 
     private void getCode() {
         try {
@@ -53,5 +102,7 @@ public class QrCode extends AppCompatActivity {
         Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
         imageViewQr.setImageBitmap(bitmap);
     }
+
+
 
 }
